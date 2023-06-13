74565038c600aa9f322df15769dea55cdc080789
Ze Mendes <zepedrosmendes@gmail.com>
2023-06-13 11:50:10 +0100

    opa ya

--- Pratical_Exam/guiao-p/ex1/Interpreter.java
+++ Pratical_Exam/guiao-p/ex1/Interpreter.java
@@ -1,10 +1,14 @@
 @SuppressWarnings("CheckReturnValue")
 public class Interpreter extends StrLangBaseVisitor<String> {
 
+   // ora criar aqui um hashmap com varriaveis e ques
+
    @Override public String visitDisplay(StrLangParser.DisplayContext ctx) {
-      String res = null;
-      return visitChildren(ctx);
-      //return res;
+      String res = visit(ctx.expression()); 
+
+      if (res != null) {
+         System.out.println(res.toString());
+      }
    }
 
    @Override public String visitAssignment(StrLangParser.AssignmentContext ctx) {
